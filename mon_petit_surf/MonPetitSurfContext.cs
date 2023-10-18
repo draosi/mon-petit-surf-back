befcfc9b1e4192764c07cab45daeb1aecb9467fb
draosi <draosi.dev@gmail.com>
2023-10-18 14:54:00 +0200

    tentative de connexion en cours...

---
+++ mon_petit_surf/MonPetitSurfContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mon_petit_surf
+{
+    public class MonPetitSurfContext : DbContext
+    {
+        public MonPetitSurfContext(DbContextOptions<MonPetitSurfContext> options)
+            : base(options)
+        {
+        }
+    }
+}
--- mon_petit_surf/Program.cs
+++ mon_petit_surf/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using mon_petit_surf;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<MonPetitSurfContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
