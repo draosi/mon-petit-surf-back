10a984ded032b407e0ea3b8b9660181910f38919
draosi <draosi.dev@gmail.com>
2023-10-24 22:03:50 +0200

    branchement avec le front

--- mon_petit_surf/Program.cs
+++ mon_petit_surf/Program.cs
@@ -1,7 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using MonPetiSurf.Context;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin().AllowAnyHeader()
+                .AllowAnyMethod();
+                      });
+});
 
 // Add services to the container
 builder.Services.AddControllers();
@@ -21,6 +33,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
