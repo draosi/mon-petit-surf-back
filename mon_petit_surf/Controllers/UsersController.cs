0653ef9dbfa31f15855b393ad95710cd57e5c029
draosi <draosi.dev@gmail.com>
2023-10-31 08:44:56 +0100

    creation users controller

---
+++ mon_petit_surf/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MonPetiSurf.Context;
+using MonPetitSurf.Services;
+
+namespace MonPetitSurf.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly MonPetitSurfContext _context;
+        protected MonPetitSurfService _monPetitSurfService { get; set; }
+
+        public UsersController(MonPetitSurfContext context)
+        {
+            _context = context;
+            _monPetitSurfService = new MonPetitSurfService(_context);
+        }
+    }
+}
